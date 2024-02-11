14c4c2b9c26559014d8e1436225673453f1244b7
Robert Stec <robertspol@gmail.com>
2024-02-11 16:04:41 +0100

    add borders in FascinatingSpace.jsx

--- src/sections/fascinating-space/FascinatingSpace.jsx
+++ src/sections/fascinating-space/FascinatingSpace.jsx
@@ -1,3 +1,123 @@
+// import { useState } from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faRocket } from "@fortawesome/free-solid-svg-icons";
+// import NavBar from "../../NavBar";
+
+// const FascinatingSpace = () => {
+//     const [inputNumber, setInputNumber] = useState("");
+//     const [fetchedData, setFetchedData] = useState([]);
+
+//     const handleInput = (e) => {
+//         setInputNumber(e.target.value);
+//     };
+
+//     const handleFetchPictures = (e) => {
+//         e.preventDefault();
+
+//         if (inputNumber === "" || inputNumber < 1) {
+//             alert("Podaj liczbę zdjęć do wyświetlenia.");
+//             return;
+//         }
+
+//         const url = `https://api.nasa.gov/planetary/apod?count=${inputNumber}&api_key=XM5v3W8bOnlG7AVUY1UXNC34P5yQeDUCMQ94hkvx
+//         `;
+
+//         fetch(url)
+//             .then((res) => {
+//                 if (!res.ok) {
+//                     throw new Error("Problem z pobraniem danych.");
+//                 }
+
+//                 return res.json();
+//             })
+//             .then((data) => {
+//                 setFetchedData(data);
+//             })
+//             .catch((err) => {
+//                 console.error(err);
+//                 alert(err);
+//             });
+//     };
+
+//     const handleDisplayPictures = () => {
+//         return fetchedData.map((item, i) => (
+//             <li key={i} className="fascinating-space__picture">
+//                 <figure>
+//                     <figcaption>
+//                         {item.title && (
+//                             <p className="fascinating-space__img-title">
+//                                 {item.title}
+//                             </p>
+//                         )}
+//                         {item.date && <p>Data: {item.date}</p>}
+//                         {item.copyright && (
+//                             <p>Prawa autorskie: {item.copyright}</p>
+//                         )}
+//                     </figcaption>
+
+//                     <img
+//                         src={item.hdurl || item.url}
+//                         className="fascinating-space__img"
+//                         alt="space"
+//                     />
+//                 </figure>
+//             </li>
+//         ));
+//     };
+
+//     return (
+//         <section className="fascinating-space">
+//             <NavBar section="fascinating-space" />
+
+//             <div className="fascinating-space__wrapper">
+//                 <h1>Magiczne zdjęcia spoza naszego podwórka</h1>
+
+//                 <p>
+//                     Podaj ilość zdjęć kosmosu jaką chcesz zobaczyć, a po
+//                     kliknięciu w rakietę zostaną losowo pobrane prosto od NASA!
+//                 </p>
+
+//                 <form className="fascinating-space__form">
+//                     <input
+//                         value={inputNumber}
+//                         type="number"
+//                         className="fascinating-space__input"
+//                         onChange={handleInput}
+//                         min="1"
+//                     />
+//                     {fetchedData.length !== 0 && (
+//                         <button
+//                             className="fascinating-space__reload-btn"
+//                             onClick={handleFetchPictures}
+//                         >
+//                             Pobierz kolejne zdjęcia
+//                         </button>
+//                     )}
+//                 </form>
+
+//                 {fetchedData.length === 0 ? (
+//                     <div className="fascinating-space__rocket-icon-wrapper">
+//                         <p>
+//                             Wsiądź do rakiety i zacznij zwiedzanie wszechświata!
+//                         </p>
+//                         <FontAwesomeIcon
+//                             icon={faRocket}
+//                             onClick={handleFetchPictures}
+//                             className="fa-beat-fade"
+//                         />
+//                     </div>
+//                 ) : (
+//                     <ul className="fascinating-space__pictures-list">
+//                         {handleDisplayPictures()}
+//                     </ul>
+//                 )}
+//             </div>
+//         </section>
+//     );
+// };
+
+// export default FascinatingSpace;
+
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRocket } from "@fortawesome/free-solid-svg-icons";
@@ -40,29 +160,33 @@ const FascinatingSpace = () => {
     };
 
     const handleDisplayPictures = () => {
-        return fetchedData.map((item, i) => (
-            <li key={i} className="fascinating-space__picture">
-                <figure>
-                    <figcaption>
-                        {item.title && (
-                            <p className="fascinating-space__img-title">
-                                {item.title}
-                            </p>
-                        )}
-                        {item.date && <p>Data: {item.date}</p>}
-                        {item.copyright && (
-                            <p>Prawa autorskie: {item.copyright}</p>
-                        )}
-                    </figcaption>
+        return fetchedData.map((item, i) =>
+            item.hdurl || item.url ? (
+                <li key={i} className="fascinating-space__picture">
+                    <figure>
+                        <figcaption>
+                            {item.title && (
+                                <p className="fascinating-space__img-title">
+                                    {item.title}
+                                </p>
+                            )}
+                            {item.date && <p>Data: {item.date}</p>}
+                            {item.copyright && (
+                                <p>Prawa autorskie: {item.copyright}</p>
+                            )}
+                        </figcaption>
 
-                    <img
-                        src={item.hdurl || item.url}
-                        className="fascinating-space__img"
-                        alt="space"
-                    />
-                </figure>
-            </li>
-        ));
+                        <img
+                            src={item.hdurl || item.url}
+                            className="fascinating-space__img"
+                            alt="space"
+                        />
+                    </figure>
+                </li>
+            ) : (
+                <li>Brak zdjęcia</li>
+            )
+        );
     };
 
     return (
